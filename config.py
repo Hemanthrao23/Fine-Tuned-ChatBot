ec54b0e1c4f9e3eaf965bea6f06238c940629e06
Hemanthrao23 <106801710+Hemanthrao23@users.noreply.github.com>
2025-05-06 12:41:44 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+# config.py
+'''
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'Hemanth1234')
+    
+    # MySQL Database URI (for mysqlclient)
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://hemanth:Hem%402000@localhost/Hem_database'
+
+
+
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', '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')
+
+'''
+import os
+from datetime import timedelta
+
+
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'Hemanth1234')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://hemanth:Hem%402000@localhost/Hem_database'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', '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')
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+
