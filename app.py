608cada9af9e8878561a5f51ce90a270a56f0fb8
Hemanthrao23 <106801710+Hemanthrao23@users.noreply.github.com>
2025-05-06 14:16:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,14 +14,6 @@ from flask_sqlalchemy import SQLAlchemy
 from config import SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-
-
-
 app.config.from_object(Config)
 db.init_app(app)
 
